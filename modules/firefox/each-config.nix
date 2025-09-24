06f82e736fa4f7d58405f46ca19f888c29e4c10c
jaredmontoya <49511278+jaredmontoya@users.noreply.github.com>
2025-09-24 12:16:27 +0200

    firefox: add reader mode themeing support

--- modules/firefox/each-config.nix
+++ modules/firefox/each-config.nix
@@ -51,6 +51,26 @@ mkTarget {
       {
         cfg,
         colors,
+      }:
+      {
+        programs.${name}.profiles = lib.mkIf (cfg.firefoxGnomeTheme.enable || cfg.colorTheme.enable) (
+          lib.genAttrs cfg.profileNames (_: {
+            settings = {
+              "reader.color_scheme" = "custom";
+              "reader.custom_colors.background" = colors.withHashtag.base00;
+              "reader.custom_colors.foreground" = colors.withHashtag.base05;
+              "reader.custom_colors.selection-highlight" = colors.withHashtag.base04;
+              "reader.custom_colors.unvisited-links" = colors.withHashtag.base0D;
+              "reader.custom_colors.visited-links" = colors.withHashtag.base0E;
+            };
+          })
+        );
+      }
+    )
+    (
+      {
+        cfg,
+        colors,
         inputs,
       }:
       {
